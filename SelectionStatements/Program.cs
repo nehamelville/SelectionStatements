69357a348873ef09843ca87460b3d28d99893ffc
nehamelville <neha.melville@gmail.com>
2021-09-29 07:27:03 -0700

    initial commit

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+            //var favNumber = 100;
+
+            Console.Write("Guess What your favourite number is :");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("Too Low");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Too High");
+
+            }
+            else if (userInput == favNumber)
+            {
+                Console.WriteLine("Congratulations!!You are right!!");
+            }
+            else
+            {
+                Console.WriteLine("Never mind!!");
+            }
+        }
+    }
+}
